3456ff631dcea390793d8cf1dd2d0f13e815dfad
noman <nomannabil30@gmail.com>
2022-09-15 15:23:01 +0600

    final commit - style changes

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const display = document.getElementById("display");
-const question = document.getElementById("question");
-const startBtn = document.getElementById("starts");
-const countdownOverlay = document.getElementById("countdown");
-const resultModal = document.getElementById("result");
-const modalBackground = document.getElementById("modal-background");
+const display = document.getElementById('display');
+const question = document.getElementById('question');
+const startBtn = document.getElementById('starts');
+const countdownOverlay = document.getElementById('countdown');
+const resultModal = document.getElementById('result');
+const modalBackground = document.getElementById('modal-background');
 
 // variables
-let userText = "";
+let userText = '';
 let errorCount = 0;
 let startTime;
-let questionText = "";
+let questionText = '';
 
 // Load and display question
-fetch("./texts.json")
+fetch('./texts.json')
   .then((res) => res.json())
   .then((data) => {
     questionText = data[Math.floor(Math.random() * data.length)];
@@ -21,12 +21,11 @@ fetch("./texts.json")
 
 // checks the user typed character and displays accordingly
 
-
 const typeController = (e) => {
   const newLetter = e.key;
   // console.log(newLetter);
   // Handle backspace press
-  if (userText.length > 0 && newLetter == "Backspace") {
+  if (userText.length > 0 && newLetter == 'Backspace') {
     errorCount++;
     userText = userText.slice(0, userText.length - 1);
     return display.removeChild(display.lastChild);
@@ -35,7 +34,7 @@ const typeController = (e) => {
 
   // these are the valid character we are allowing to type
   const validLetters =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890!@#$%^&*()_+-={}[]'\".,?";
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890!@#$%^&*()_+-={}[]\'".,?';
 
   // if it is not a valid character like Control/Alt then skip displaying anything
   if (!validLetters.includes(newLetter)) {
@@ -47,9 +46,13 @@ const typeController = (e) => {
   const newLetterCorrect = validate(newLetter);
 
   if (newLetterCorrect) {
-    display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
+    display.innerHTML += `<span class="green">${
+      newLetter === ' ' ? '▪' : newLetter
+    }</span>`;
   } else {
-    display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    display.innerHTML += `<span class="red">${
+      newLetter === ' ' ? '▪' : newLetter
+    }</span>`;
   }
 
   // check if given question text is equal to user typed text
@@ -67,24 +70,26 @@ const validate = (key) => {
 
 // FINISHED TYPING
 const gameOver = (errorCount) => {
-  document.removeEventListener("keydown", typeController);
+  document.removeEventListener('keydown', typeController);
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
   const timeTaken = (finishTime - startTime) / 1000;
 
   // show result modal
-  resultModal.innerHTML = "";
-  resultModal.classList.toggle("hidden");
-  modalBackground.classList.toggle("hidden");
+  resultModal.innerHTML = '';
+  resultModal.classList.toggle('hidden');
+  modalBackground.classList.toggle('hidden');
   // clear user text
-  display.innerHTML = "";
+  display.innerHTML = '';
   // make it inactive
-  display.classList.add("inactive");
+  display.classList.add('inactive');
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken ? Math.trunc(timeTaken) : 0}</span> seconds</p>
+    <p>You took: <span class="bold">${
+      timeTaken ? Math.trunc(timeTaken) : 0
+    }</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
@@ -94,13 +99,13 @@ const gameOver = (errorCount) => {
   // restart everything
   startTime = null;
   errorCount = 0;
-  userText = "";
-  display.classList.add("inactive");
+  userText = '';
+  display.classList.add('inactive');
 };
 
 const closeModal = () => {
-  modalBackground.classList.toggle("hidden");
-  resultModal.classList.toggle("hidden");
+  modalBackground.classList.toggle('hidden');
+  resultModal.classList.toggle('hidden');
   window.location.reload(true);
 };
 
@@ -109,7 +114,7 @@ const start = () => {
   if (startTime) return;
 
   let count = 3;
-  countdownOverlay.style.display = "flex";
+  countdownOverlay.style.display = 'flex';
 
   const startCountdown = setInterval(() => {
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
@@ -117,21 +122,20 @@ const start = () => {
     // finished timer
     if (count == 0) {
       // -------------- START TYPING -----------------
-      document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "none";
-      display.classList.remove("inactive");
+      document.addEventListener('keydown', typeController);
+      countdownOverlay.style.display = 'none';
+      display.classList.remove('inactive');
 
       clearInterval(startCountdown);
 
       startTime = new Date().getTime();
-    
     }
     count--;
   }, 1000);
 };
 
 // START Countdown
-startBtn.addEventListener("click", start);
+startBtn.addEventListener('click', start);
 
 // If history exists, show it
 displayHistory();
@@ -141,8 +145,9 @@ setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
 
-
-  document.getElementById("show-time").innerHTML = `${startTime ? Math.trunc(timeSpent) : 0} seconds`;
+  document.getElementById('show-time').innerHTML = `${
+    startTime ? Math.trunc(timeSpent) : 0
+  } seconds`;
 }, 1000);
 
 // Stop page scrolling when space is pressed
